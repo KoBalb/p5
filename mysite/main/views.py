5490ff29a5a983ddd5309b992803d2d47e5c6d3a
KoBalb <artem.kovalyk79@gmail.com  artem.kovalyk79@gmail.com>
2025-03-30 18:36:40 +0300

    complete

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
-
+from main.forms import TextForm
+from main.models import Text
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    form = TextForm()
+
+    if request.method == 'POST':
+        form = TextForm(request.POST)
+        if form.is_valid():
+            texts = form.save()
+
+    texts = Text.objects.all()
+    data = {
+        'form' : form,
+        'texts': texts,
+    }
+    return render(request, 'main/index.html', data)
